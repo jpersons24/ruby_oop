f207bf45a1047da0e5e514cd0d5becc93383225b
Jakob Persons <jakobep24@gmail.com>
2022-10-13 15:55:19 -0400

    instance and class methods

--- oop/app/models/user.rb
+++ oop/app/models/user.rb
@@ -1,16 +1,70 @@
 class User
-    attr_reader :age # attr_reader writes getter/reader methods for you
-    attr_accessor :name, :bio # attr_accessor gives both getter/reader methods and setter methods
+
+    attr_reader :views
+    attr_accessor :name, :bio, :hair_color, :birthdate, :age, :home_state
+    @@all = []
 
     # name, age, bio
     def initialize(name, age, bio)
         @name = name
         @age = age
         @bio = bio
+        @hair_color = "red"
+        @birthdate = nil
+        @views = 0
+        @home_state = ""
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_home_state(state)
+        return self.all.select { |user| user.home_state == state }
+    end
+
+    def self.most_popular
+        views_per_user =  self.all.map { |user| user.views }
+        most_popular_user = self.all.filter { |user| user.views == views_per_user.max}
+        puts "#{most_popular_user[0].name} has the most profile views"
     end
 
     def view_profile
-        puts "You are #{@name}, #{@age} years old and all about #{@bio}"
+        increment_views
+        puts "You are #{self.name}, #{self.age} years old and all about #{self.bio}"
+        puts "Your profile has been viewed #{self.views} times"
+    end
+
+    def celebrate_birthday
+        self.age += 1
+        puts "Happy birthday, you are now #{self.age} years old"
+    end
+
+    def of_drinking_age
+        if self.age >= 21
+            puts "#{self.name} can legally drink in the United States."
+        else
+            puts "Sorry, #{self.name}, you are not old enough to legally drink yet."
+        end
+    end
+
+    def of_driving_age
+        if self.age >= 16
+            puts "Awesome, #{self.name} you can legally drive!"
+        else
+            if self.name == "Gunnar"
+                puts "#{self.name}, you are a dog...you can never drive"
+                return
+            end
+            puts "Sorry, #{self.name} you have to wait #{16 - self.age} years before you can drive"
+        end
+    end
+
+    private
+
+    def increment_views
+        @views += 1
     end
 
 end
--- oop/run.rb
+++ oop/run.rb
@@ -16,30 +16,38 @@ def run
 
     if choice == 'y'
         new_user.view_profile
+        puts "Would you like to change your bio?"
+        second_choice = gets.chomp
+
+        if second_choice == "y"
+            puts "Go ahead and update your bio"
+            updated_bio = gets.chomp
+            new_user.bio = updated_bio
+            puts "Would you like to see your new profile?"
+            answer = gets.chomp
+
+            if answer =="y"
+                new_user.view_profile
+            else
+                puts "See you later!"
+            end
+
+        else
+            puts "See you later!"
+        end
+
     else 
         puts "See you later"
     end 
-
-    puts "Would you like to change your bio?"
-    second_choice = gets.chomp
-    if second_choice == "y"
-        
-        puts "Go ahead and update your bio"
-        updated_bio = gets.chomp
-        new_user.bio = updated_bio
-        puts "would you like to see your new profile"
-        answer = gets.chomp
-
-        if answer == "y"
-            puts "You are #{new_user.name}, you are #{new_user.age} years old, and you are all about #{new_user.bio}"
-        else
-            puts "come back another time"
-        end
-
-    else
-        puts "No problem see you later"
-    end
 end
+
+jake = User.new("Jake", 30, "coding and soccer")
+brynn = User.new("Brynn", 30, "instagram and my dog")
+chase = User.new("Chase", 26, "hamptons and my boyfriend")
+erin = User.new("Erin", 24, "taking care of animals")
+jack = User.new("Jack", 18, "playing fortnite and being the cool kid at school")
+kinsley = User.new("Kinsley", 4, "playing with magnet tiles and dressing like a princess")
+gunnar = User.new("Gunnar", 1, "playing with my toys and annoying my dad")
 
 binding.pry
 puts 'prying complete'
